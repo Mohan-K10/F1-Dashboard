962caaa3b4c447cf3eec403ac61c21430f091e9d
Mohan-K10 <mohank18m@gmail.com>
2026-08-01 22:26:50 +0530

    style(dashboard): set main background color to true black for consistency

--- client/f1_dashboard/src/components/Dashboard.jsx
+++ client/f1_dashboard/src/components/Dashboard.jsx
@@ -4,12 +4,12 @@ import Seasons from '../pages/Seasons'
 
 const Dashboard = () => {
   return (
-    <div className='min-h-screen bg-[#131318] px-4 py-4 text-white sm:px-6 lg:px-8'>
-        <div className='mx-auto min-h-screen w-full max-w-7xl'>
-            <Navbar/>
-            <Seasons/>
-            
-        </div>
+    <div className='min-h-screen bg-black px-4 py-4 text-white sm:px-6 lg:px-8'>
+      <div className='mx-auto min-h-screen w-full max-w-7xl'>
+        <Navbar />
+        <Seasons />
+
+      </div>
     </div>
   )
 }
